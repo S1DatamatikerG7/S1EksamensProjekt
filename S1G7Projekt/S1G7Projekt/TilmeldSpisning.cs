d477f7f4fa1c05f803079bc64576456940b2daf5
rebelricco <riccojorgensen@gmail.com>
2016-12-06 13:45:55 +0100

    AddedTilmeldSpisning.cs

---
+++ S1G7Projekt/S1G7Projekt/TilmeldSpisning.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1G7Projekt
+{
+    class TilmeldSpisning
+    {
+        public int[,] TilmeldArray { get; set; }
+
+
+        public TilmeldSpisning()
+        {
+            TilmeldArray = new int[4,4];
+
+        }
+
+
+
+
+    }
+}
