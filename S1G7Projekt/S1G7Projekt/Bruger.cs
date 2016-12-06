7ad9ae5bc4ea843d5edcc546c2e149504d66af9b
rebelricco <riccojorgensen@gmail.com>
2016-12-06 11:08:25 +0100

    Added Bruger.cs

---
+++ S1G7Projekt/S1G7Projekt/Bruger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1G7Projekt
+{
+    class Bruger
+    {
+        public int BrugerID { get; set; }
+        public String HusNr { get; set; }
+        public String Kodeord { get; set; }
+
+
+        public Bruger()
+        {
+            
+        }
+
+        public Bruger(int brugerId, string husNr, string kodeord)
+        {
+            BrugerID = brugerId;
+            HusNr = husNr;
+            Kodeord = kodeord;
+        }
+
+    }
+}
