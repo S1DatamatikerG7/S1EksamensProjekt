5499b94f47496aafdd8dce224073d40255e48d68
Lardman <Nicki@sonic.dk>
2016-12-06 12:33:32 +0100

    Json template added inc. filenames

--- S1G7Projekt/S1G7Projekt/FileHandler.cs
+++ S1G7Projekt/S1G7Projekt/FileHandler.cs
@@ -8,5 +8,33 @@ namespace S1G7Projekt
 {
     class FileHandler
     {
+        private static string LoginFileName = "Login.json";
+        private static string BetalingsListeFileName = "BetalingsListe.json";
+        private static string TilmeldSpisningFileName = "TilmeldSpisning.json";
+        private static string NaesteUgeFileName = "NaesteUge.json";
+
+        public static async void SaveNoteAsJsonAsync(ObservableCollection<Login> notes)
+        {
+            string NoteAsJsonString = JsonConvert.SerializeObject(notes);
+            SerializeNotesFileAsync(NoteAsJsonString, jsonFileName);
+        }
+
+        public static async Task<ObservableCollection<NoteModel>> LoadNoteFromJsonAsync()
+        {
+            string notesJsonString = await DeserializeNotesFileAsync(jsonFileName);
+            return (ObservableCollection<NoteModel>)JsonConvert.DeserializeObject(notesJsonString, typeof(ObservableCollection<NoteModel>));
+        }
+
+        public static async void SerializeNotesFileAsync(string noteString, string fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(localFile, noteString);
+        }
+
+        public static async Task<string> DeserializeNotesFileAsync(string fileName)
+        {
+            StorageFile localFile = await ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+            return await FileIO.ReadTextAsync(localFile);
+        }
     }
 }
