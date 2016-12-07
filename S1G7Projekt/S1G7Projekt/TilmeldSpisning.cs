4e2a2553af0b998c3b55aed4d1931ec538b5e77c
rebelricco <riccojorgensen@gmail.com>
2016-12-07 10:08:21 +0100

    Updated TilmeldSpisning

--- S1G7Projekt/S1G7Projekt/TilmeldSpisning.cs
+++ S1G7Projekt/S1G7Projekt/TilmeldSpisning.cs
@@ -8,14 +8,37 @@ namespace S1G7Projekt
 {
     class TilmeldSpisning
     {
-        public int[,] TilmeldArray { get; set; }
+        public int MaVoksen { get; set; }
+        public int TiVoksen { get; set; }
+        public int OnVoksen { get; set; }
+        public int ToVoksen { get; set; }
 
+        public int MaBorn7_15 { get; set; }
+        public int TiBorn7_15 { get; set; }
+        public int OnBorn7_15 { get; set; }
+        public int ToBorn7_15 { get; set; }
+
+        public int MaBorn3_6 { get; set; }
+        public int TiBorn3_6 { get; set; }
+        public int OnBorn3_6 { get; set; }
+        public int ToBorn3_6 { get; set; }
+
+        public int MaBorn2_0 { get; set; }
+        public int TiBorn2_0 { get; set; }
+        public int OnBorn2_0 { get; set; }
+        public int ToBorn2_0 { get; set; }
 
         public TilmeldSpisning()
         {
-            TilmeldArray = new int[4,4];
 
         }
+
+        public void GemTilmelding()
+        {
+            Filhandler.Tilmelding(MaVoksen, MaBorn7_15, MaBorn3_6, MaBorn2_0, TiVoksen, TiBorn7_15, TiBorn3_6, TiBorn2_0, OnVoksen, OnBorn7_15, OnBorn3_6, OnBorn2_0, ToVoksen, ToBorn7_15, ToBorn3_6, ToBorn2_0)
+        }
+
+
 
 
 
