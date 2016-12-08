75a811643cd9a337d763962f23d30a308ce407c9
rebelricco <riccojorgensen@paradis.dk>
2016-12-08 11:56:07 +0100

    Added load til tiilmelding

--- S1G7Projekt/S1G7Projekt/VMTilmeldSpisning.cs
+++ S1G7Projekt/S1G7Projekt/VMTilmeldSpisning.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Windows.Devices.Bluetooth.Advertisement;
+using NoteModelOpg33;
 
 namespace S1G7Projekt
 {
@@ -12,7 +15,6 @@ namespace S1G7Projekt
 
         public String Dag { get; set; }
 
-        public List<String> HusNr { get; set; }
 
         public int SelectedHus { get; set; }
 
@@ -21,8 +23,22 @@ namespace S1G7Projekt
         public int AntalBorn3_6 { get; set; }
         public int AntalBornU3 { get; set; }
 
+
+
+
+        public List<String> HusNr { get; set; }
         public Dictionary<String, List<string>> InfoDictionary;
         public List<String> InputInfo;
+
+
+        private RelayCommand _relayCommandGem;
+
+        public RelayCommand RelayCommandGem
+        {
+            get { return _relayCommandGem; }
+            set { _relayCommandGem = value; }
+        }
+
 
         public VMTilmeldSpisning()
         {
@@ -30,29 +46,43 @@ namespace S1G7Projekt
             SelectedHus = 0;
             InfoDictionary = new Dictionary<string, List<string>>();
             InputInfo = new List<string>();
+
+
+
+
+            _relayCommandGem = new RelayCommand(GemTilmelding);
         }
 
         public void GemTilmelding()
         {
-            if (Dag != null)
+            if (Dag != null || HusNr != null)
             {
-                if (HusNr != null)
+                if (AntalVoksne != 0 || AntalBorn7_15 != 0 || AntalBorn3_6 != 0 || AntalBornU3 != 0)
                 {
-                    if (AntalVoksne != 0 || AntalBorn7_15 != 0 || AntalBorn3_6 != 0 || AntalBornU3 != 0)
-                    {
-                        InputInfo.Add($"{Dag}");
-                        InputInfo.Add($"{AntalVoksne}");
-                        InputInfo.Add($"{AntalBorn7_15}");
-                        InputInfo.Add($"{AntalBorn3_6}");
-                        InputInfo.Add($"{AntalBornU3}");
+                    InputInfo.Add($"{Dag}");
+                    InputInfo.Add($"{AntalVoksne}");
+                    InputInfo.Add($"{AntalBorn7_15}");
+                    InputInfo.Add($"{AntalBorn3_6}");
+                    InputInfo.Add($"{AntalBornU3}");
 
-                        InfoDictionary.Add(HusNr[SelectedHus], InputInfo);
+                    InfoDictionary.Add(HusNr[SelectedHus], InputInfo);
 
 
-                        FileHandler.Tilmelding(InfoDictionary);
-                    }
+                    FileHandler.Tilmelding(InfoDictionary);
+                }
+                else
+                {
+                    //TODO: Exception
                 }
             }
+        }
+
+
+        public Dictionary<string, List<string>> LoadTilmelding()
+        {
+            //TODO: Load
+
+            return null;
         }
     }
 }
